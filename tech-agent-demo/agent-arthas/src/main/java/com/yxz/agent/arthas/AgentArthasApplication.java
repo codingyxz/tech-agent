99074acb9f62001c0b696d1d29d603f1bb6b5603
light <zxya9527@163.com>
2025-07-07 14:23:31 +0800

    move tech-agent-demo tech-asm

---
+++ tech-agent-demo/agent-arthas/src/main/java/com/yxz/agent/arthas/AgentArthasApplication.java
@@ -0,0 +1,13 @@
+package com.yxz.agent.arthas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentArthasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgentArthasApplication.class, args);
+    }
+
+}
